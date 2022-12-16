7308ea61d1c95a7b9c93894be6ae4c3fb90a902f
HuyDang <huy.dang.quoc@vieon.vn>
2022-12-16 16:23:52 +0700

    update theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,6 +38,9 @@ require('vscode').setup({
     group_overrides = {
         -- this supports the same val table as vim.api.nvim_set_hl
         -- use colors from this colorscheme by requiring vscode.colors!
-        Cursor = { fg=c.vscDarkBlue, bg=c.vscYellowOrange, bold=true },
+		    Cursor = { fg=c.vscWhite, bg=c.vscBlack, bold=true },
+		    ["@keyword"] = { fg = c.vscBlue, bg = c.vscNone },
+        ["@include"] = { fg = c.vscBlue, bg = c.vscNone },
+        ["@keyword.return"] = { fg = c.vscPink, bg = c.vscNone }
     }
 })
