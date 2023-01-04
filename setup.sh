0129d01d2363c29a7f1a36b0ea1510ae7049590f
huydang <dangqhuy2411@gmail.com>
2023-01-04 18:45:16 +0700

    setup.sh: fix install nvim

--- setup.sh
+++ setup.sh
@@ -2,31 +2,36 @@
 
 CURRENT_VERSION_NVIM=""
 
-
 build_prerequisites()
 {
-	if [ "$OSTYPE" = "linux-gnu"* ] ; then
-		 echo "you chose choice 1"
+	if [ "$OSTYPE" = "linux-gnu" ] ; then
+		 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
 	fi
 }
 
 pre_install_nvim()
 {
 	echo "Checking current version"
-	CURRENT_VERSION=nvim --version
+	CURRENT_VERSION=$(nvim --version)
 
 	echo "Build prerequisites"
 	echo "Your current OS: $OSTYPE"
+	build_prerequisites
+	
 }
 
 post_install_nvim()
 {
    echo "Install dependentcies"
 	 #lazygit
+	 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"v*([^"]+)".*/\1/')
+	 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	 sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+	 lazygit --version
+	 #	------
 	 #delta git
-
 	 echo "Applying current config"
-	 cp /nvim $HOME/.config/nvim
+	 rsync -a nvim/ $HOME/.config/nvim
    echo "Clear cache"
 	 rm -rf neovim
 }
@@ -37,16 +42,15 @@ install_nvim()
 	 pre_install_nvim
    echo "Downloading latest version"
 	 git clone https://github.com/neovim/neovim
-	 cd neovim
-	 git checkout stable
-	 make CMAKE_BUILD_TYPE=Release
-	 sudo make install 
+	 echo "Installing  on path: $PWD"
+	 (cd neovim && git checkout stable && make distclean; make CMAKE_BUILD_TYPE=Release && sudo make install) 
 	 nvim --version
+	 post_install_nvim
 }
 
 install_zsh()
 {
-		echo "START INSTALL ZSH"
+    echo "START INSTALL ZSH"
    	echo "Downloading OMZ"
 		sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 		post_install_zsh
@@ -56,33 +60,31 @@ post_install_zsh()
 {
    echo "Install dependentcies"
 	 echo "Install zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 	 echo "Applying current config"
 	 cp zsh/.zshrc $HOME 
 	 source $HOME/.zshrc
 }
 
+
 PS3='Please enter your choice: '
-options=("Auto" "Install neovim" "Install alacritty" "Quit")
+options=("Install zsh" "Install neovim" "Install alacritty" "Quit")
 select opt in "${options[@]}"
 do
 	case $opt in
-        "Auto")
-            echo "you chose choice 1"
-            ;;
-        "Install zsh")
+				"Install zsh")
 					install_zsh
-            ;;
-        "Install neovim")
+						;;
+				"Install neovim")
 					install_nvim
-            ;;
-        "Install alacritty")
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
+						;;
+				"Install alacritty")
+						;;
+				"Quit")
+						break
+						;;
+				*) echo "invalid option $REPLY";;
+		esac
 done
 
 
