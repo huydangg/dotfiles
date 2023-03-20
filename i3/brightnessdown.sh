fb69527bf02f09e08e8e0a48b38a982fd6a387f0
Huy Dang <huy.dang.quoc@vieon.vn>
2023-03-20 16:01:21 +0700

    i3: adjust brighness and show on i3bar

---
+++ i3/brightnessdown.sh
@@ -0,0 +1,3 @@
+# /bin/bash
+
+brightnessctl s 5%- | awk 'BEGIN { "brightnessctl g" | getline g; close("brightnessctl g");"brightnessctl m" | getline m; close("brightnessctl m"); print (g/m) *100 }' > ~/.config/brightness
